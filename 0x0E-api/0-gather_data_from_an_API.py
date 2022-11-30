dab5577dfd231cbf21e849f49c39eb0118031d7b
Vernon-444 <mt.vernon44@gmail.com>
2022-11-30 14:54:17 -0800

    push 0 and fixed pep8 violations

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,15 +1,13 @@
-# 0-gather_data_from_an_API
+import sys
+import requests
 
-if __name__ == "__main__":
-    import sys
-    import requests
 
-    def get_employee_todo():
+def get_employee_todo():
         """Gets info from JSON Placeholder API, generates fake data for
         Employee Name, Employee To Do, Employee Completed To Do
         Based on input of an Employee ID as an integer"""
-        employee_id = sys.arg[1]
-        completed = 0
+        employee_id = sys.argv[1]
+        num_completed = 0
         source = "https://jsonplaceholder.typicode.com/"
 
         employee_name = (requests.get(f'{source}users/{employee_id}')).json()
@@ -18,5 +16,24 @@ if __name__ == "__main__":
 
         # testing above code so far
 
-        print(f'Employee Name Result: {employee_name}')
-        print(f'Employee_to_to Result: {employee_to_do}')
+        # print(f'Employee Name Result: {employee_name}')
+        # print(f'Employee_to_to Result: {employee_to_do}')
+
+        completed_list = []
+
+        for task in employee_to_do:
+            if task.get("completed") is True:
+                num_completed += 1
+                completed_list.append(task.get("title"))
+
+        # print(num_completed)
+        # print(len(employee_to_do))
+
+        name = employee_name.get("name")
+        print(f'Employee {name} is done with'
+              f'tasks({num_completed}/{len(employee_to_do)}):')
+        for task in completed_list:
+            print(f'\t {task}')
+
+if __name__ == "__main__":
+    get_employee_todo()
