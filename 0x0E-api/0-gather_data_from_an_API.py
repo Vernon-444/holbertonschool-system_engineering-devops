1b176f17377ed49327f4a157da78b4c6a6177790
Vernon-444 <mt.vernon44@gmail.com>
2022-11-30 15:27:02 -0800

    restore code with minor change

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,5 +1,6 @@
-import sys
-import requests
+#!/usr/bin/python3
+"""Gets info about employee from an API"""
+
 
 def get_employee_todo():
         """Gets info from JSON Placeholder API, generates fake data for
@@ -10,8 +11,6 @@ def get_employee_todo():
         source = "https://jsonplaceholder.typicode.com/"
 
         employee_name = (requests.get(f'{source}users/{employee_id}')).json()
-        employee_to_do = (requests.get(f'{source}'
-                                       f'todos?userID={employee_id}')).json()
 
         # testing above code so far
 
@@ -29,7 +28,8 @@ def get_employee_todo():
         # print(len(employee_to_do))
 
         name = employee_name.get("name")
-        print(f'Employee {name} is done with' tasks({num_completed}/{len(employee_to_do)}):')
+        print(f'Employee {name} is done with'
+              f'tasks({num_completed}/{len(employee_to_do)}):')
         for task in completed_list:
             print(f'\t {task}')
 
